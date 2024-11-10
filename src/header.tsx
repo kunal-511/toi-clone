1727be307b0dafeae1a1239e82075025254a88d5
kunal Dugar <yoyokvunal@gmail.com>
2024-11-10 20:46:44 +0530

    removed bug
    Signed-off-by: kunal Dugar <yoyokvunal@gmail.com>

--- src/header.tsx
+++ src/header.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 import MainBanner from "./mainBanner";
 import Navbar from "./navbar";
-import HighlightNews from './highlightNews';
+import HighlightNews from "./highlightNews";
 const Header: React.FC = () => {
-    const handleapi = () => {
-      fetch("https://invalid-api-url.com/data")
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-      // No catch block, so Sentry will catch the rejection
-    };
+  const handleapi = () => {
+    fetch("https://invalid-api-url.com/data")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    // No catch block, so Sentry will catch the rejection
+  };
 
   return (
     <>
       <MainBanner />
       <div style={{ backgroundColor: "Green", height: "100px" }}>
         <h1>Welcome to my App</h1>
-        <button onClick={() => methodDoesNotExist()}>Break the world</button>
       </div>
 
       <div style={{ backgroundColor: "pink", height: "100px" }}>
